9c17be727cbe39b658336161c02cd82e39257573
mahanunu <mahalia.pires@gmail.com>
2024-04-15 17:53:48 +0200

    css gameboy

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="images/favicon.png">
     <link rel="stylesheet" href="css/style.css">
     <title>Pokédex</title>
 </head>
@@ -34,6 +33,133 @@
 
         
     </main>
+    <div class="gameboy" id="GameBoy">
+  
+  <div class="screen-area">
+    
+    <div class="power">
+      <div class="indicator">
+        <div class="led"></div>
+        <span class="arc" style="z-index:2"></span>
+        <span class="arc" style="z-index:1"></span>
+        <span class="arc" style="z-index:0"></span>
+      </div>
+      POWER
+    </div>
+    
+    <canvas class="display" id="mainCanvas"></canvas>
+    
+    <div class="label">
+      <div class="title">GAME BOY</div>
+      <div class="subtitle">
+        <span class="c">C</span><!--
+     --><span class="o1">O</span><!--
+     --><span class="l">L</span><!--
+     --><span class="o2">O</span><!--
+     --><span class="r">R</span>
+      </div>
+    </div>
+    
+  </div>
+  
+  <div class="nintendo">Nintendo</div>
+  
+  <div class="controls">
+    <div class="dpad">
+      <div class="up"><i class="fa fa-caret-up"></i></div>
+      <div class="right"><i class="fa fa-caret-right"></i></div>
+      <div class="down"><i class="fa fa-caret-down"></i></div>
+      <div class="left"><i class="fa fa-caret-left"></i></div>
+      <div class="middle"></div>
+    </div>
+    <div class="a-b">
+      <div class="b">B</div>
+      <div class="a">A</div>
+    </div>
+  </div>
+  
+  <div class="start-select">
+    <div class="select">SELECT</div>
+    <div class="start">START</div>
+  </div>
+  
+  <div class="speaker">
+    <div class="dot placeholder"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot placeholder"></div>
+    
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+
+    <div class="dot placeholder"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot closed"></div>
+    <div class="dot open"></div>
+    <div class="dot placeholder"></div>
+  </div>
+  
+</div>
+
+    
     <script src="js/script.js"></script>
 </body>
 </html>
